a5fe843f28c94dabeed8807ba7e1d20723632e2e
Otávio Santiago <otaviosatago@gmail.com>
2025-02-22 11:24:07 -0300

    melhorei o layout da seção de partituras

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,3 +7,21 @@ function SR(id) {
     const section = document.getElementById(id)
     section.scrollBy({left:400, behavior: 'smooth'})
 }
+
+async function loadMusicXML() {
+    const response = await fetch('./public/parts/partitura.musicxml'); // Busca o arquivo na pasta pública
+    const xmlText = await response.text();
+
+    const osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay("osmd-container", {
+        drawingParameters: "compacttight",
+    });
+    await osmd.load(xmlText);
+    osmd.setOptions({
+        scale: 0.5,
+        spacingFactor: 0.6  
+    });
+    osmd.render();
+}
+
+// Chama a função ao carregar a página
+window.onload = loadMusicXML;
